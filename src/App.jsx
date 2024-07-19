af4220b17759d3e7f64bcf3dfc5e7ff41796ff12
Nadika <nadika.name@gmail.com>
2024-07-19 15:37:35 +0100

    feat: add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   
@@ -9,8 +7,8 @@ function App() {
   return (
    <div className="calculator-grid">
     <div className="output">
-      <div className="previous-operand"></div>
-      <div className="current-operand"></div>
+      <div className="previous-operand">123,123</div>
+      <div className="current-operand">123,123</div>
     </div>
     <button className="span-two">AC</button>
     <button>DEL</button>
@@ -35,4 +33,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
