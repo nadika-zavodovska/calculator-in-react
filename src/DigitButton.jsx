279746073b46d9db9f68284883b805130d04cacb
Nadika Zavodovska <nadika.name@gmail.com>
2024-08-16 14:46:40 +0100

    Update DigitButton.jsx

--- src/DigitButton.jsx
+++ src/DigitButton.jsx
@@ -1,1 +1,5 @@
+import { ACTIONS } from './App'
 
+export deafault function DigitButton({ dispatch, digit }) {
+  return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>{digit}</button>
+}
